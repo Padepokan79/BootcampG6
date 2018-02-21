06454638a1010e38e64a457a44e9de2935bac424
fahrejaabdullah <fahrejaabdullah@gmail.com>
2018-02-21 13:59:53 +0700

    add file 15 19 20 21 Feb

---
+++ Fahreja/20 Feb/Bilangan.java
@@ -0,0 +1,10 @@
+public class Bilangan{
+	public static void main(String[] args) {
+		int bilangan ;
+
+		for (bilangan = 30 ; bilangan <= 90 ; bilangan += 15) {
+			System.out.println(bilangan);
+		}
+	}
+	
+}
