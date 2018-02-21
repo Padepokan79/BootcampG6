88dfd960b198d15f5daad7ffab653c51c6272adb
regiwitanto <witantoregi@gmail.com>
2018-02-21 14:06:45 +0700

    Tambahkan file tgl 13 Februari

---
+++ Regi/13feb2018/GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt {
+	public static void main( String[] args ) {
+		System.out.println( "+-----------------------+" );
+		System.out.println( "|                       |" );
+		System.out.println( "|        PERTAMINA      |" );
+		System.out.println( "|                       |" );
+		System.out.println( "| 2018-02-12   00:12AM  |" );
+		System.out.println( "|                       |" );
+		System.out.println( "| Premium   :  Rp 6500  |" );
+		System.out.println( "| Pertamax  :  Rp 7600  |" );
+		System.out.println( "|                       |" );
+		System.out.println( "| Fuel Total: Rp 13000  |" );
+		System.out.println( "|                       |" );
+		System.out.println( "+-----------------------+" );
+	}
+}
