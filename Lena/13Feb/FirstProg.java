2c029356ec4461e3269a3fbf228f8a84002b0a95
lenawidiani <magdalenawidiani@gmail.com>
2018-02-21 13:45:14 +0700

    menambahkan file tgl 13 feb

---
+++ Lena/13Feb/FirstProg.java
@@ -0,0 +1,8 @@
+public class FirstProg{
+	public static void main( String[] args )
+	{
+		System.out.println("I, Magdalena WR , am determined to learn how to code");
+		System.out.println("Today's date is Tuesday, February 13th, 2018");
+	}
+
+}
