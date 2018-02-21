1796786e207c6aa64fd9d4ddf254f28e8b501f6e
fahrejaabdullah <fahrejaabdullah@gmail.com>
2018-02-21 14:06:50 +0700

    add file 13 Feb

---
+++ Fahreja/13 Feb/GasolineReceipt.java
@@ -0,0 +1,14 @@
+public class GasolineReceipt{
+	public static void main( String[] args){
+		System.out.println("+---------------------------+");
+		System.out.println("|                           |");
+		System.out.println("|         PERTAMINA         |");
+		System.out.println("|   2018-02-13 14:28WIB     |");
+		System.out.println("|                           |");
+		System.out.println("|   PREMIUM :      3 Liter  |");
+		System.out.println("|  Harga/Lt :  Rp.7.500,00- |");
+		System.out.println("| Fuel total: Rp.22.500,00- |");
+		System.out.println("|                           |");
+		System.out.println("+---------------------------+");
+	}
+}
