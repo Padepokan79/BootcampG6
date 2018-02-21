4a5fcfad93baa2d94e1f6a4cb693cf3a62486e03
lenawidiani <magdalenawidiani@gmail.com>
2018-02-21 13:56:41 +0700

    menambahkan file tgl 14 feb

---
+++ Lena/14Feb/HarapanLena.java
@@ -0,0 +1,7 @@
+public class HarapanLena{
+	public static void main(String[] args) {
+		System.out.println("Harapan dalam jangka pendek:\n- Dapat memiliki banyak teman\n- Dapat memiliki penghasilan sendiri\n- Dapat semakin memahami dan menerapkan: Java, .net, dll\n- Dapat menjalani Bootcamp dengan baik\n- Dapat menjalani OJT dengan baik\n");
+		System.out.println("Harapan dalam jangka panjang:\n- Dapat mengerjakan proyek-proyek dengan baik\n- Dapat ditempatkan pada posisi yang sesuai dengan kemampuan saya\n- Dapat memiliki pengalaman di banyak posisi selain programmer\n- Penghasilan meningkat\n- Dapat berlibur dengan penghasilan sendiri\n");
+		System.out.println("Harapan saya pada CV. 79:\n- Dapat mengembangkan diri saya dari segi hard skill\n- Dapat mengembangkan diri saya dari segi soft skill\n- Saya dapat ditempatkan kepada posisi yang sesuai dengan kemampuan saya\n- Dapat memiliki pengalaman di banyak posisi selain sebagai programmer\n- Dapat memiliki banyak teman sehingga dapat mengembangkan diri dan wawasan saya");
+	}
+}
