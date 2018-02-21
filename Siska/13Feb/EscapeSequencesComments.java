8ffea2333d8728bdacf1e2f27f24e4cc00eb2043
unknown <siska.pitriani16039@gmail.com>
2018-02-21 13:48:47 +0700

    Add file 13Feb

---
+++ Siska/13Feb/EscapeSequencesComments.java
@@ -0,0 +1,18 @@
+public class EscapeSequencesComments{
+	public static void main (String[]args){
+		// This exercise demontrates escape sequences & comments (like these)!
+		System.out.println("Learn\tJava\n\tthe\nHard\tWay\n\n");
+		System.out.println("\tLearn Java the \"Hard\" Way!\n");
+		// System.out.frimp("Learn Java Hard Way");
+		System.out.print("Hello\n"); // This line prints Hello.
+		System.out.print("Jello\by\n"); // This line prints Jelly.
+		/* The quick brown fox jumped over a lazy dog.
+		   Quick wafting zephyrs vex bold Jim. */
+		System.out /*Testing*/ .println ("Hard to bealieve, eh?");
+		System.out.println("Surprised? /* abcd */ Or what did you expert?");
+		System.out.println("\\ // -=- \\//");
+		System.out.println("\\\\ \\\\\\ \\\\\\\\"); // it take 2 to make 1
+		System.out.print("I hope understand \"escape sequences\" now. \n");
+		//and comments. :)
+	}
+}
