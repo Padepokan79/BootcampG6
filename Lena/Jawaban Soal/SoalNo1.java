892c6f50e51114fdbe2d687e40589b3a3a080d2f
lenawidiani <magdalenawidiani@gmail.com>
2018-03-06 18:32:07 +0700

    loglena6Mar2018-2

---
+++ Lena/Jawaban Soal/SoalNo1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class SoalNo1{
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		int batas, counter;
+
+		System.out.println("1. Buatlah bilangan prima hingga batas yang ditentukan.");
+		System.out.print("Batas: ");
+		batas = input.nextInt();
+
+
+		for(int i = 1 ; i <= batas ; ++i){
+
+			counter = 0;
+
+			for(int j = 1 ; j <= i ; ++j){
+				if(i%j == 0){
+					++counter;
+				}
+			}
+
+			if(counter == 2){
+				System.out.print(i+" ");
+			}
+		}
+	}
+}
