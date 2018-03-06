f92eaa3450d675c5df2b33ad18ce5b40c5e5f4da
kudismetal <raito_rider@rocketmail.com>
2018-03-06 16:27:55 +0700

    LogYudhis-06-03-2018-2

---
+++ Yudhis/6mar2018/LatSoal/LatSoal3Yudhis.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class LatSoal3Yudhis {
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		Random rand = new Random();
+
+		int [] array = new int [9];
+		int indeks = 0;
+
+		for (int i = 0; i < 9; i++) { // Memasukkan nilai acak
+			array[i] = rand.nextInt(10);
+		}
+
+		for (int i = 0; i < 3; i++) { // Membuat baris
+			for (int j = 0; j < 3; j++) { // Membuat kolom
+				System.out.print(array[indeks] + " ");
+				indeks++;
+			}
+
+			System.out.println();
+		}
+	}
+}
