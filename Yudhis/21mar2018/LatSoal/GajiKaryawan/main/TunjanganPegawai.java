b65043f85aae181610cb9f9dbf4dd271c51a4d9a
kudismetal <raito_rider@rocketmail.com>
2018-03-21 15:37:31 +0700

    LogYudhis-21-03-2018

---
+++ Yudhis/21mar2018/LatSoal/GajiKaryawan/main/TunjanganPegawai.java
@@ -0,0 +1,13 @@
+package main;
+
+public interface TunjanganPegawai {
+
+	int tunjanganPegawaiJunior = 200000;
+	int tunjanganPegawaiMiddle = 300000;
+	int tunjanganPegawaiSenior = 500000;
+	
+	int tunjanganTransportasiJakarta = 1000000;
+	
+	double satuanTunjanganKeluarga = 0.02;
+	
+}
