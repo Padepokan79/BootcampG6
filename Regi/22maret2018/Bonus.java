1ce59b04590ec684851fcad6fa048761ee574b6f
regiwitanto <witantoregi@gmail.com>
2018-03-22 15:56:40 +0700

    LOG_REGI_22032018

---
+++ Regi/22maret2018/Bonus.java
@@ -0,0 +1,7 @@
+package Karyawan;
+
+public interface Bonus {
+	int bonusProgrammer = 20000;
+	int bonusTester = 25000;
+
+}
---
+++ Regi/22maret2018/UMK.java
@@ -0,0 +1,8 @@
+package Karyawan;
+
+public interface UMK {
+	int umkKotaBandung = 3091345;
+	int umkKotaJakarta = 3919291;
+	int umkKabKarawang = 3648035;
+
+}
