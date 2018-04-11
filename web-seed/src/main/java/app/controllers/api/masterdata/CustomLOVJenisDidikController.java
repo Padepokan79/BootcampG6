dbbce5c49c5207116632b1a2fdd43c87a6509822
ilhamgalang <ilhamgalang62@gmail.com>
2018-04-11 16:34:49 +0700

    Tambah file CustomLOVJenisDidikController CustomLOVJenisPegController

---
+++ web-seed/src/main/java/app/controllers/api/masterdata/CustomLOVJenisDidikController.java
@@ -0,0 +1,24 @@
+package app.controllers.api.masterdata;
+
+import java.util.List;
+import java.util.Map;
+
+import app.models.MdJenisDidik;
+import core.io.model.LOVModel;
+import core.io.model.PagingParams;
+import core.javalite.controllers.LOVController;
+
+public class CustomLOVJenisDidikController extends LOVController<MdJenisDidik> {
+	
+	public void initListOfValueModel(LOVModel model) {
+		model.setLovKey("jnsdidik");
+		model.setLovValues("nmjenisdidik");
+	}
+	
+	public List<Map<String, Object>> customOnLoad(PagingParams params) throws Exception {
+		params.setFilter("jnsdidik < ?", 8);
+		params.setOrderBy("nmjenisdidik");
+		
+		return super.customOnLoad(params);
+	}
+}
